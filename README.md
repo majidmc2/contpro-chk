6877770d0925f4b4437239f0c42af75d13e73ae2
majidmc2 <majidmc2@localhost.localdomain>
2020-04-21 05:48:49 -0400

    complete the script

---
+++ README.md
@@ -0,0 +1,32 @@
+
+
+
+## About
+This script helps you on Android protesting when you want to send queries to some ***Android Content-Provider***. This script can check a URI or a list of them and also if you are looking for a special text, it can show the output by grabbing it.
+
+----
+## Installation
+####Requirements
+> Python3.*
+
+####Run
+At first you should clone the repositoty
+> git clone 
+
+Then run contpro-chk.py by **Python3.***
+> cd contpro-chk
+> python3.7 contpro-chk.py --help
+
+Output of help:
+
+![](help.png)
+
+----
+
+## TODO
+
+- [x] Send Query
+    - [x] a URI
+    - [x] a list of URIs
+- [x] Grab a special text in the output
+- [ ] Find all content-providers in smali codes
---
+++ contpro-chk.py
@@ -0,0 +1,59 @@
+import os
+import argparse
+
+
+def logo():
+    my_log = """
+-------------------------------------------------------------------------
+ ██████  ██████  ███    ██ ████████ ██████  ██████   ██████             ██████ ██   ██ ██   ██ 
+██      ██    ██ ████   ██    ██    ██   ██ ██   ██ ██    ██           ██      ██   ██ ██  ██  
+██      ██    ██ ██ ██  ██    ██    ██████  ██████  ██    ██           ██      ███████ █████   
+██      ██    ██ ██  ██ ██    ██    ██      ██   ██ ██    ██           ██      ██   ██ ██  ██  
+ ██████  ██████  ██   ████    ██    ██      ██   ██  ██████   ███████   ██████ ██   ██ ██   ██ 
+                                                                                                                                                                     
+                    Owner: Majid Iranpour
+                     twitter: @_majidmc2
+-------------------------------------------------------------------------
+"""
+    print(my_log)
+
+
+def connection(uri, grep=None):
+    if grep:
+        cmd = 'adb shell content query --uri {uri} | grep -i "{grep}"'.format(uri=uri, grep=grep)
+    else:
+        cmd = 'adb shell content query --uri {uri}'.format(uri=uri)
+    print('uri ==> ', uri, '\n', os.system(cmd), '\n', '====================')
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="cont_chk",
+                                     usage="python3 cont_chk.py [options]",
+                                     description="""This script checks all of your Android Content-Providers and 
+                                     shows output of them""")
+    parser.add_argument('-u', nargs='?', type=str, help='A spacial URI')
+    parser.add_argument('-l', nargs='?', type=str, help='List of URIs in a txt format file')
+    parser.add_argument('-g', nargs='?', type=str, help='Grep on a spacial name and if it find then show output')
+    args = parser.parse_args()
+
+    if args.u:
+        logo()
+        if args.g:
+            connection(args.u, args.g)
+        else:
+            connection(args.u)
+    elif args.l:
+        try:
+            with open(args.l, 'r') as f:
+                logo()
+                for uri in f.readlines():
+                    if args.g:
+                        connection(uri, args.g)
+                    else:
+                        connection(uri)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
